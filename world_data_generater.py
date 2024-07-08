c7dea5315b60930f971ffc93b623b2d928bb9b76
Chad DeRosier <chad.derosier@tutanota.com>
2024-07-07 19:13:52 -0700

    relative positions

--- world_data_generater.py
+++ world_data_generater.py
@@ -49,10 +49,11 @@ def world_to_string(worlds):
         for world in world_set:
             text += ','.join([str(v) for v in flatten_3d(world.amoeba.smell_from_things(world.things))]) + ','
         world = world_set[0]
+        amoeba = world.amoeba
         text += f'{str(len(world.things))},'
         count = len(world.things)
         for t in world.things:
-            text += ','.join([str(v) for v in [t.x, t.y, t.smell[0], t.smell[1], t.smell[2]]]) + ','
+            text += ','.join([str(v) for v in [amoeba.x - t.x, amoeba.y - t.y, t.smell[0], t.smell[1], t.smell[2]]]) + ','
         text += ','.join(['0,0,0,0,0' for _ in range(8 - count)])
         text = text + '\n'
 
@@ -139,7 +140,6 @@ def do_it(rows_min=12, cols_min=12, rows_max=30, cols_max=30, runs=10, max_thing
             'x7,y7,r7,g7,b7\n' \
 
     text += world_to_string(worlds)
-    print(world_to_string(worlds))
 
     text = text[:-1]
 
@@ -150,5 +150,5 @@ def do_it(rows_min=12, cols_min=12, rows_max=30, cols_max=30, runs=10, max_thing
         print(text)
 
 if __name__ == "__main__":
-    do_it(runs=4, should_print=False)
+    do_it(runs=10000, should_print=False, filename="training_data.csv")
     
